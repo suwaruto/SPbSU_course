b58be03d01abc144ee5e1f010e9452ece9c142fa
Ovinnikov Nikita <gondork12@gmail.com>
2021-04-25 01:10:58 +0300

    Shared object task has been added. Using sorting algorithms as an example.

---
+++ Shared_Object/arrayio.h
@@ -0,0 +1,2 @@
+void init_random( int *arr, int arr_size, int higher_bound );
+int print_random_subarray( int *arr, int arr_size, int subarr_size );
---
+++ Shared_Object/sort.h
@@ -0,0 +1,6 @@
+void bubble_sort( int *arr, int arr_size );
+void insertion_sort( int *arr, int arr_size );
+void merge_sort( int *arr, int arr_size );
+void heap_sort( int *arr, int arr_size );
+void quicksort( int *arr, int arr_size );
+
